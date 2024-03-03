b63502b2cdd2502e4d782e5341730c93f841f0ff
Chanikya K <157685732+Chanu716@users.noreply.github.com>
2024-03-03 12:02:44 +0530

    Update implementation_of_stack_using_array.c

--- implementation_of_stack_using_array.c
+++ implementation_of_stack_using_array.c
@@ -1,55 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 10
 
-#define Max 10
+int  stack[MAX];
+int top=-1;
 
-int stack[Max];
-int top = -1;
-
-void push(int value) {
-    if (top == Max - 1) {
-        printf("Error: Stack overflow!\n");
-        return;
-    }
-    stack[++top] = value;
+int isFull(){
+if(top==MAX-1){
+    return 1;
+}
+else{ 
+    return 0;
+}
 }
 
-int pop() {
-    if (top == -1) {
-        printf("Error: Stack underflow!\n");
-        return -1;
-    }
-    return stack[top--];
+int isEmpty(){
+if(top==-1){
+    return 1;
+}
+else{ 
+    return 0;
+}
 }
 
-int main() {
-    int choice, value;
+void push(int value)
+{
+if (isFull()==1){
+    printf("Stack Overflow");
+    return;
+}
+else{
+    top++;
+    stack[top]=value;
+}
+}
 
-    while (1) {
-        printf("1. Push\n2. Pop\n3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+int pop()
+{int item;
+if(isEmpty()){
+    printf("Stack Underflow"); 
+    return -1;
+}
+else{
+    item=stack[top];
+    top--;
+    return item;
+}
+}
 
-        switch (choice) {
+void peek(){
+if(isEmpty()){
+    printf("Underflow");
+}
+else{
+    printf("Top element is %d",stack[top]);
+}
+}
+
+void display()
+{if(isEmpty()){
+    printf("Underflow");
+}
+else{
+    for(int i=0;i<top+1;i++)
+    printf("%d ",stack[i]);
+}
+}
+
+int main(){
+int choice,value;
+    while(1){
+        printf("\nMENU\n");
+        printf("1.Push\n2.Pop\n3.Peek\n4.Display\n5.Exit\n");
+        printf("\nEnter your choice:");
+        scanf("%d",&choice);
+
+        switch(choice){
             case 1:
-                printf("Enter value to push: ");
-                scanf("%d", &value);
+                printf("Enter value to push:");
+                scanf("%d",&value);
                 push(value);
                 break;
-
             case 2:
-                value = pop();
-                if (value != -1) {
+                value=pop();
+                if(value!=-1) {
                     printf("Popped value: %d\n", value);
                 }
                 break;
-
             case 3:
-                exit(0);
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                exit(1);
 
             default:
-                printf("Invalid choice!\n");
+                printf("Invalid choice.Try again"); 
         }
+        printf("\n");
     }
-    return 0;
+    return 0;                
 }
